36264111bbdddee5d106effcc55c645565a1b5ef
Иван Гнедько <40666716+FemFX@users.noreply.github.com>
2021-08-21 20:25:33 +0300

    Create track.controller.ts

---
+++ server/src/track/track.controller.ts
@@ -0,0 +1,56 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Query,
+  UploadedFiles,
+  UseInterceptors,
+} from '@nestjs/common';
+import { FileFieldsInterceptor } from '@nestjs/platform-express';
+import { ObjectId } from 'mongoose';
+import { AddCommentDto } from './dto/add-comment.dto';
+import { CreateTrackDto } from './dto/create-track.dto';
+import { TrackService } from './track.service';
+
+@Controller('track')
+export class TrackController {
+  constructor(private trackService: TrackService) {}
+  @Post()
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'picture', maxCount: 1 },
+      { name: 'audio', maxCount: 1 },
+    ]),
+  )
+  create(@UploadedFiles() files, @Body() dto: CreateTrackDto) {
+    const { picture, audio } = files;
+    return this.trackService.create(dto, picture[0], audio[0]);
+  }
+  @Get()
+  getAll(@Query('count') count: number, @Query('offset') offset: number) {
+    return this.trackService.getAll(count, offset);
+  }
+  @Get('search')
+  search(@Query('query') query: string) {
+    return this.trackService.search(query);
+  }
+  @Get(':id')
+  getOne(@Param('id') id: ObjectId) {
+    return this.trackService.getOne(id);
+  } 
+  @Delete(':id')
+  delete(@Param('id') id: ObjectId) {
+    return this.trackService.delete(id);
+  }
+  @Post('comment')
+  addComment(@Body() dto: AddCommentDto) {
+    return this.trackService.addComment(dto);
+  }
+  @Post('listen/:id')
+  listen(@Param('id') id: ObjectId) {
+    return this.trackService.listen(id);
+  }
+}
